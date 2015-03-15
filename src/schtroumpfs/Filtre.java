45e0ee3bc909b83ddcd454159ae817f0dce2bafd
Baptiste Charrier <baptistech@hotmail.fr>
2015-03-15 21:24:17 +0100

    Changement de la classe Filtre en interface

--- src/schtroumpfs/Filtre.java
+++ src/schtroumpfs/Filtre.java
@@ -1,18 +1,16 @@
 package schtroumpfs;
 
 /*
- * Classe de Filtre à Schtroumpf.
+ * Interface Filtre à Schtroumpf.
  * @author Baptiste CHARRIER
  * @version 1
  */
-public class Filtre<Schtroumpf> {
+public interface Filtre<Schtroumpf> {
 	
 	/*
-	 * Accepte un schtroumpf.
+	 * Méthode d'interface implicitement abstraite redéfinie dans les classes de test acceptant un schtroumpf.
 	 * @return true si le schtroumpf est accepté.
 	 */
-	public boolean accepte(Schtroumpf s) {
-		return true;
-	}
+	public boolean accepte(Schtroumpf s) ;
 
 }
